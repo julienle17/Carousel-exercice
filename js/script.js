c2d49134f38705d79ae6ec3a462c3df7430ddf94
julienle17 <julienleclercq17@gmail.com>
2019-04-10 09:54:51 +0200

    add displayNone class in html & css and create function nextImg in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict'
+
+let count = 0;
+let listImages = document.getElementsByClassName('carousel');
+let lengthImages = listImages.length;
+
+function nextImg() {
+	if (count < lengthImages -1) {
+	listImages[count].classList.add('displayNone');
+	listImages[count+1].classList.remove('displayNone');
+	count++;
+
+	} else {
+	listImages[0].classList.remove('displayNone');
+	listImages[lengthImages -1].classList.add('displayNone');
+	count = 0;
+	}
+}
+
+function previousImg() {
+
+}
